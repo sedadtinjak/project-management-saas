13f429989ee7c8d5a44c9211093f3a29c3162fcb
Sedad <sedadtinjak@gmail.com>
2025-12-03 17:35:33 +0100

    Add gitignore and initial backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) =>
+    {
+        res.send("Backend is running");
+    });
+
+app.listen(5000, () =>
+    {
+        console.log("Server is running on http://localhost:5000");
+    });
